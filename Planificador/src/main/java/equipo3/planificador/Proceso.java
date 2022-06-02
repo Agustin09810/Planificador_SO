4f50b250582ebca91837cf80048d7e303b9643f8
Agustin09810 <a.lorenzogon2@gmail.com>
2022-06-01 22:35:24 -0300

    Primer Commit

---
+++ Planificador/src/main/java/equipo3/planificador/Proceso.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package equipo3.planificador;
+import java.util.Date;
+import java.util.Calendar;
+/**
+ *
+ * @author Administrador
+ */
+public class Proceso {
+    int id;
+    enum Tipo{
+        TIEMPOREAL,
+        INTERACTIVOS,
+        BATCH
+    }
+    int prioridad;
+    Lc
+   
+}
